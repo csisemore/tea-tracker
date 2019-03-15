ac22fefe152c269c6e6951253d32a88788c68c68
csisemore <camilla.sisemore@gmail.com>
2019-03-14 22:34:59 -0500

    Fixed application properties

--- src/main/java/com/example/teatracker/models/data/BrandDao.java
+++ src/main/java/com/example/teatracker/models/data/BrandDao.java
@@ -1,11 +1,13 @@
 package com.example.teatracker.models.data;
 
-
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import javax.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+//import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-@Transactional
+
 public class BrandDao extends CrudRepository<Brand, Integer>{
     public Object findAll() {
     }
