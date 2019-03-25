7dfdd82183bf59989b055ee9a42751174127e6f1
csisemore <camilla.sisemore@gmail.com>
2019-03-25 12:01:23 -0500

    Changes at home 20190325

--- src/main/java/com/example/teatracker/controllers/BrandController.java
+++ src/main/java/com/example/teatracker/controllers/BrandController.java
@@ -1,11 +1,16 @@
 package com.example.teatracker.controllers;
 
-
+import com.example.teatracker.models.Brand;
 import com.example.teatracker.models.data.BrandDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("brand")
@@ -15,11 +20,30 @@ public class BrandController {
 
     @RequestMapping(value = "")
     public  String index(Model model){
-        model.addAttribute("title", "Brands");
-        //model.addAttribute("brands", brandDao.findAll());
-        model.addAttribute("brand_name", brandDao.findAll());
-
+        model.addAttribute("title", "Tea Brands");
+        model.addAttribute("brand", brandDao.findAll());
 
         return "brand/index";
     }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+          public String displayAddBrandForm(Model model){
+              model.addAttribute("title", "Add Brand");
+              model.addAttribute(new Brand());
+
+              return "brand/add";
+         }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddBrandForm(@ModelAttribute @Valid Brand brand, Errors errors, Model model){
+        if (errors.hasErrors()){
+                  model.addAttribute("title", "Add Brand");
+                  return "brand/add";
+              }
+        brandDao.save(brand);
+        return "redirect:";
+    }
+
+
 }
+
--- src/main/java/com/example/teatracker/controllers/TeaController.java
+++ src/main/java/com/example/teatracker/controllers/TeaController.java
@@ -1,23 +1,58 @@
 package com.example.teatracker.controllers;
 
+import com.example.teatracker.models.Tea;
+import com.example.teatracker.models.data.BrandDao;
 import com.example.teatracker.models.data.TeaDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("tea")
 public class TeaController {
+
     @Autowired
     private TeaDao teaDao;
+
+    @Autowired
+    private BrandDao brandDao;
+//    private Integer brandId;
 
     @RequestMapping(value = "")
     public  String index(Model model){
         model.addAttribute("title", "Teas");
-        model.addAttribute("brands", teaDao.findAll());
+        model.addAttribute("tea", teaDao.findAll());
 
         return "tea/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddTeaForm(Model model){
+        model.addAttribute("title", "Add Tea");
+        model.addAttribute(new Tea());
+
+        return "tea/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddTeaForm(@ModelAttribute @Valid Tea tea, Errors errors, Model model){
+        if (errors.hasErrors()){
+            model.addAttribute("title", "Add Brand");
+            return "tea/add";
+        }
+
+
+//        Integer brand_id;
+//        Brand brand = brandDao.findOne(id);
+        teaDao.save(tea);
+        return "redirect:";
+
     }
 }
 
