b2213fc1cce462475989c21b5adb5721612af0a6
csisemore <camilla.sisemore@gmail.com>
2019-04-08 17:26:17 -0500

    Fixed the bad git merge.

--- src/main/java/com/example/teatracker/models/Tea.java
+++ src/main/java/com/example/teatracker/models/Tea.java
@@ -84,3 +84,95 @@ public class Tea {
 //        return teaTypes;
 //    }
 }
+
+
+
+/***
+package com.example.teatracker.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Tea {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+
+    @NotNull
+    @Size(min=2, max=25)
+    //@Size(min=2)
+    private String name;
+
+
+// out until I figure this out.
+    //@NotNull
+//    @Size()
+//    private Boolean like_dislike;
+
+    @ManyToOne
+    private Brand brand;
+
+//    @ManyToMany(mappedBy = "teas")
+//    private List<TeaType> teaTypes;
+
+
+
+    public Tea(String name) {
+
+        this.name = name;
+    }
+
+    public Tea() {
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+
+        this.id = id;
+    }
+
+
+
+    public Brand getBrand() {
+
+        return brand;
+    }
+
+
+    public void setBrand(Brand brand) {
+
+        this.brand = brand;
+    }
+
+
+//    public Boolean getLike_dislike() {
+//        return like_dislike;
+//    }
+//
+//    public void setLike_dislike(Boolean like_dislike) {
+//        this.like_dislike = like_dislike;
+//    }
+
+//    public List<TeaType> getTeaType(){
+//        return teaTypes;
+//    }
+}
+
+ ***/
